5fc779deef384d1de2d3cea59d8e66ff8e86f408
anas <anasrabb2000@gmail.com>
2022-08-15 19:07:15 +0100

    filter component added

--- client/src/components/FilterComponent.jsx
+++ client/src/components/FilterComponent.jsx
@@ -1,10 +1,29 @@
-const FilterComponent = () => {
 
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
-    return ( 
+function FilterComponent() {
+    return (
+        <>
+            <Dropdown>
+                <h4>Engineer type</h4>
+                <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
+                    Software Engineering
+                </Dropdown.Toggle>
 
+                <Dropdown.Menu variant="dark">
+                    <Dropdown.Item href="#/action-1" active>
+                        Software Engineering
+                    </Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Data Science</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">Cloud Engineer</Dropdown.Item>
+                    <Dropdown.Divider />
+                </Dropdown.Menu>
+            </Dropdown>
 
-     );
+        </>
+
+    );
 }
 
 export default FilterComponent;
--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,4 +1,5 @@
 import CardsComponent from "./CardsComponent";
+import FilterComponent from "./FilterComponent";
 import ProfileCardComponent from "./ProfileCardComponent";
 function Main() {
     return (
@@ -10,11 +11,14 @@ function Main() {
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus ipsam perspiciatis quod eligendi provident eos molestias nam ea, nostrum, facilis eum rem. Cupiditate quaerat modi beatae incidunt laudantium officiis nobis!</p>
 
             </div>
+
             <br>
             </br>
-
+            <div style={{ display: 'flex', justifyContent: 'left', padding: '40px', borderRadius: '5px', backgroundColor: "white", }}>
+                <FilterComponent />
+            </div>
             <div style={{ display: 'flex', justifyContent: 'space-around', padding: '20px', borderRadius: '5px', backgroundColor: "white" }}>
-                {/* <FilterComponent/> */}
+
                 <ProfileCardComponent />
                 <ProfileCardComponent />
                 <ProfileCardComponent />
