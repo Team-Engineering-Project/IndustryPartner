eac445034f8169dadb11605aeeee0aa16580362c
Rafee Ahmed <raffe.ahmed@hotmail.co.uk>
2022-08-17 14:32:11 +0100

    cleaned up main component

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -38,14 +38,13 @@ function Main()
             <div className="header" style={{ maxWidth: '980px' }}>
                 <h1>Talent spotlight</h1>
                 <p style={{ color: "grey" }}>At Digital Futures we're focused on improving diversity within the technology sector, helping organisations build high-performing technology teams representative of society. Below is a selection of our engineers who have recently graduated from the Digital Academy and are immediately available to join your organisation </p>
-                <br>
-                </br>
-                </div>
-                <div style={{ justifyContent: 'left', padding: '45px', borderRadius: '5px', backgroundColor: "white" }}>
-                    <p style={{ color: "grey" }}>Engineer type:</p>
-                    <FilterComponent filterDropdown={filterDropdown}/>
-                </div>
-                <div style={{ display: 'flex', justifyContent: 'space-around', padding: '20px', borderRadius: '5px', backgroundColor: "white" }}>
+                <br></br>
+            </div>
+            <div style={{ justifyContent: 'left', padding: '45px', borderRadius: '5px', backgroundColor: "white" }}>
+                <p style={{ color: "grey" }}>Engineer type:</p>
+                <FilterComponent filterDropdown={filterDropdown}/>
+            </div>
+            <div style={{ display: 'flex', justifyContent: 'space-around', padding: '20px', borderRadius: '5px', backgroundColor: "white" }}>
                 {graduates && <ProfileCardsComponent graduates={graduates} />}
             </div>
 
