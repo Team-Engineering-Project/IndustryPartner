3a6e782c7161ff949e96c50ca9163ffe035fc234
Alistair Gibb <aligibb_uk@hotmail.com>
2022-08-17 15:26:20 +0100

    Add ProfileCardComponent tests

---
+++ client/src/__tests__/ProfileCardComponent.test.js
@@ -0,0 +1,41 @@
+import { render, screen } from '@testing-library/react';
+
+import ProfileCardComponent from '../components/ProfileCardComponent';
+
+import testData from '../../testGraduates.js';
+
+const graduate = testData.graduates[0]
+
+describe(`ProfileCardComponent tests`, () => {
+
+
+    describe(`render tests`, () => {
+
+        beforeEach(() => {
+            render(<ProfileCardComponent graduate={graduate} />);
+        })
+
+        test(`should render the graduate's full name`, () => {
+            expect(screen.getByText(`${graduate.firstName} ${graduate.lastName}`)).toBeInTheDocument();
+        });
+
+        test(`should render the test graduate's 'dfGaduation' date`, () => {
+            expect(screen.getByText(graduate.dfGraduationDate)).toBeInTheDocument();
+        })
+
+        test(`should render the graduates 'universityDegree'`, () => {
+            expect(screen.getByText(graduate.universityDegree)).toBeInTheDocument();
+        })
+
+        test(`should render the graduate's 'university' key`, () => {
+            expect(screen.getByText(graduate.university)).toBeInTheDocument();
+        })
+
+        test(`should render an <img> tag with the src attribute 'logo.png'`, () => {
+            const profileImg = screen.getByRole('img');
+            expect(profileImg).toHaveAttribute('src', 'logo.png');
+        })
+
+    })
+
+})
