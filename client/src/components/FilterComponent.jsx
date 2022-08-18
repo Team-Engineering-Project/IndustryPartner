20ade72e290b1c4c0f392cdcd9e4491c747443d8
Rafee Ahmed <raffe.ahmed@hotmail.co.uk>
2022-08-18 02:02:15 +0100

    adjusted filter drop down width

--- client/src/components/FilterComponent.jsx
+++ client/src/components/FilterComponent.jsx
@@ -6,7 +6,7 @@ function FilterComponent({filterDropdown})
 {
     return (
         <>
-            <select onChange={(e)=>filterDropdown(e.target.value)} className="form-select" aria-label="Default select example" style={{ width: '20%' }}>
+            <select onChange={(e)=>filterDropdown(e.target.value)} className="form-select" aria-label="Default select example" style={{ width: '30%' }}>
                 <option value="" selected>Select Engineering type</option>
                 <option value="SE">Software Engineering</option>
                 <option value="DS">Data Science</option>
