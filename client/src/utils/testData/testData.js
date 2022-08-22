3b4f257f1e838ddb0857c9cbc0fd60105c570395
Alistair Gibb <aligibb_uk@hotmail.com>
2022-08-22 12:03:26 +0100

    Add updated testData

---
+++ client/src/utils/testData/testData.js
@@ -0,0 +1,152 @@
+const testData = {
+    graduate: {
+        "firstName": "Joe",
+        "lastName": "Bloggs",
+        "profileImage": "link/to/image.jpg",
+        "pronouns": "he/him",
+        "dfSubject": "SE",
+        "dfGraduationDate": "June 2022",
+        "universityDegree": "English",
+        "university": "Brighton University",
+        "nationality": "British",
+
+        headline: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor dignissim convallis aenean et tortor at risus viverra adipiscing.",
+
+        diligence: {
+            disclosure: true,
+            rightToWork: true,
+            identity: true,
+            education: true,
+            credit: true
+
+
+        },
+
+        socialMedia: {
+            github: "github/myurl",
+            linkedIn: "linkedin/myurl"
+        },
+
+        personalityType: {
+            type: "ESTJ",
+            description: "ESTJ's are dkfdkfdkf "
+        },
+
+
+        qualifications: [
+            {
+                level: "Degree",
+                subject: "English",
+                establishment: "University of Brighton"
+            },
+            {
+                level: "A-Level",
+                subject: "English",
+                establishment: "Brighton College"
+            },
+            {
+                level: "A-level",
+                subject: "Math",
+                establishment: "Brighton College"
+            },
+            {
+                level: "A-Level",
+                subject: "French",
+                establishment: "Brighton College"
+            }
+        ],
+
+        achievements: [
+            {
+                title: "Learnt French"
+            },
+            {
+                title: "Gymnast champion"
+            }
+        ],
+
+        technicalSkills: [
+            {
+                skill: "Debugging"
+            },
+            {
+                skill: "Testing"
+            },
+            {
+                skill: "Object-Orientated Programming"
+            },
+            {
+                skill: "Pair programmer"
+            }
+        ],
+
+        feedback: [
+            {
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor dignissim convallis aenean et tortor at risus viverra adipiscing.",
+                person: "Peer"
+            },
+            {
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor dignissim convallis aenean et tortor at risus viverra adipiscing.",
+                person: "Peer"
+            },
+            {
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor dignissim convallis aenean et tortor at risus viverra adipiscing.",
+                person: "Trainer"
+            },
+            {
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor dignissim convallis aenean et tortor at risus viverra adipiscing.",
+                person: "Trainer"
+            }
+        ],
+
+        flair: [
+            {
+                type: "Hobby",
+                description: "Guitar"
+            },
+            {
+                type: "Hobby",
+                description: "Writing"
+            },
+            {
+                type: "Hobby",
+                description: "Cooking"
+            },
+            {
+                type: "Favourite book",
+                description: "Mrs Dalloway"
+            }
+        ],
+
+        trainingProgress: [
+            {
+                module: "Fundamentals",
+                challenge: "Javascript Fundamentals Cahllenge",
+                result: "Pass"
+            },
+            {
+                module: "Software Design",
+                challenge: "Test-driven development",
+                result: "Pass"
+            },
+            {
+                module: "Software Development 1",
+                challenge: "3rd-Party integration",
+                result: "Pass"
+            },
+            {
+                module: "Software Developments 2",
+                challenge: "Sever Side Development (NodeJS) Challenge",
+                result: "Pass"
+            },
+            {
+                module: "Team Engineering",
+                challenge: "Agile Team",
+                result: "Pass"
+            }
+
+        ]
+    }
+}
+
+export default testData;
