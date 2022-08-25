3603a1cd60d5ea94b86b7741e8839fd02ec86634
Rafee Ahmed <raffe.ahmed@hotmail.co.uk>
2022-08-25 05:32:38 +0100

    talent spotlight badges added and cohort signature

--- client/src/components/TalentSpotlight/ProfileCard.jsx
+++ client/src/components/TalentSpotlight/ProfileCard.jsx
@@ -1,8 +1,9 @@
 
-import logo from './img/ProfileImg.png'
+import profilePlaceholder from './img/profile-placeholder.jpeg'
 import graduationIcon from './img/mortarboard-fill.svg';
 import degreeIcon from './img/briefcase-fill.svg';
-import Button from 'react-bootstrap/Button';
+import badgeIcon from './img/badge-icon.svg'
+//import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -12,26 +13,47 @@ function ProfileCard({ graduate }) {
             {/* <Card.Img variant="top" src={logo} style={{ width: '100px', padding: '10px', borderRadius: '200px' }} /> */}
             <Card.Body style={{ paddingLeft:'3px', paddingRight:'3px'}}>
                 <div className="img+title" style={{ display: 'flex', justifyContent: 'flex-start' }}>
-                    <Card.Img variant="top" src={logo} style={{ width: '78px', padding: '0px 10px 10px 0px', borderRadius: '200px' }} />
-                    <Card.Title>{graduate.firstName} {graduate.lastName}<br />  <p style={{ color: 'grey', fontSize: '15px' }}>{graduate.pronouns}</p> </Card.Title>
+                    <Card.Img variant="top" src={profilePlaceholder} style={{ width: '80px', padding: '0px 10px 10px 0px', borderRadius: '200px' }} />
+                    <Card.Title>
+                        {graduate.firstName} {graduate.lastName}
+                        <br/> 
+                        <p className="m-0" style={{ color: 'grey', fontSize: '15px' }}>{graduate.pronouns}</p>
+                        <p className="m-0" style={{ color: 'grey', fontSize: '15px', paddingTop:'3px' }}>{graduate.dfSubject}</p>  
+                    </Card.Title>
                 </div>
                 <Card.Text>
                     <div className="graduation">
-                        <Card.Img src={graduationIcon} style={{ width: '10%', height: 'auto'}}/>
+                        <Card.Img src={graduationIcon} style={{ width: '9%', height: 'auto'}}/>
                         <h5 style={{display: 'inline', paddingLeft:'10px', fontSize: '1rem'}}>Digital Futures graduation</h5>
-                        <p style={{paddingLeft: '2.2em', color:'grey', fontSize: '1rem'}}>{graduate.dfGraduationDate}</p>
+                        <p style={{paddingLeft: '2em', color:'grey', fontSize: '1rem'}}>{graduate.dfGraduationDate}</p>
                     </div>
                     <div className='Education'>
                         <Card.Img src={degreeIcon} style={{ width: '8%', height: 'auto'}}/>
                         <h5 style={{display: 'inline', paddingLeft:'12px', fontSize: '1rem'}} >{graduate.universityDegree}</h5>
-                        <p style={{paddingLeft: '2.05em', color:'grey', fontSize: '1rem'}}>{graduate.university}</p>
+                        <p style={{paddingLeft: '2.03em', color:'grey', fontSize: '1rem'}}>{graduate.university}</p>
                     </div>
-                    {/* { graduate.OracleCertifiedAssociateScore />} */}
-                    <hr className='footer' style={{width:'16em'}}/>
+                    {graduate.badges.java8 ? 
+                    <div className='Badges-Java'>
+                        <Card.Img src={badgeIcon} style={{ width: '9%', height: 'auto'}}/>
+                        <h5 style={{display: 'inline', paddingLeft:'8px', fontSize: '1rem'}} >Oracle Java SE 8 Associate</h5>
+                        <p style={{paddingLeft: '2.05em', color:'grey', fontSize: '1rem'}}>{graduate.badges.java8}</p>
+                    </div> : <></>}
+                    {graduate.badges.python ? 
+                    <div className='Badges-Python'>
+                        <Card.Img src={badgeIcon} style={{ width: '9%', height: 'auto'}}/>
+                        <h5 style={{display: 'inline', paddingLeft:'8px', fontSize: '1rem'}} >Python Associate</h5>
+                        <p style={{paddingLeft: '2.05em', color:'grey', fontSize: '1rem'}}>{graduate.badges.python}</p>
+                    </div> : <></>}
+                    {graduate.badges.aws ? 
+                    <div className='Badges-Aws'>
+                        <Card.Img src={badgeIcon} style={{ width: '9%', height: 'auto'}}/>
+                        <h5 style={{display: 'inline', paddingLeft:'8px', fontSize: '1rem'}} >AWS Solutions Associate</h5>
+                        <p style={{paddingLeft: '2.05em', color:'grey', fontSize: '1rem'}}>{graduate.badges.aws}</p>
+                    </div> : <></>}
                 </Card.Text>
             </Card.Body>
-            <Card.Footer>
-                <Button className='viewProfileButton' variant="primary" style={{ alignItems: 'centergit', width: '100%' }}>View profile</Button>
+            <Card.Footer style={{padding: '10px 0px 10px 0px', backgroundColor:'white'}}>
+                <button type='button' className='btn p-2' style={{ color:'#3B8BC9',alignItems: 'centergit', width: '100%' }}><strong>View profile</strong></button>
             </Card.Footer>
         </Card >
     )
